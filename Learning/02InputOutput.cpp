1bd4c95abfcc8752c807c0565528c86cd565c309
Bhavy-Kakaniya <bhavy2kakaniya@gmail.com>
2025-10-31 21:18:46 +0530

    input output

---
+++ Learning/02InputOutput.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2;
+    cout << "Enter value of number 1: "; // << is inserton operator
+    cin >> num1;                         // >> is extraction operator
+    cout << "Enter value of number 2: ";
+    cin >> num2;
+    cout << "Sum is " <<num1 + num2;
+    return 0;
+}
