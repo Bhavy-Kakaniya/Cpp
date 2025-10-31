c319ba3e79dc07e6c51a53e1e1feae3cd2c3908c
Bhavy-Kakaniya <bhavy2kakaniya@gmail.com>
2025-10-31 21:16:59 +0530

    basic structure of cpp

---
+++ Learning/01BasicStructure.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main(){
+    std::cout<<"Hello World";
+    return 0;
+}
+// with and without namespace
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hello World";
+    return 0;
+}
