5aae1f95dbcc6638a8d93ad5a6b8aec96d7edacb
Bhavy-Kakaniya <bhavy2kakaniya@gmail.com>
2025-11-02 23:45:30 +0530

    convert feet to inches

---
+++ Practice/02DescisionMaking/01FeetToInches.cpp
@@ -0,0 +1,12 @@
+// convert feet to inches
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int feet;
+    cout << "Enter Feet: ";
+    cin >> feet;
+    cout << feet * 12 << " inches";
+}
