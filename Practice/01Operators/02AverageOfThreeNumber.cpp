0dabb6998ca48216cbd649f279ffb7b7e5908e72
Bhavy-Kakaniya <bhavy2kakaniya@gmail.com>
2025-11-02 23:42:57 +0530

    average of three numbers

---
+++ Practice/01Operators/02AverageOfThreeNumber.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a, b, c;
+    cout << "Enter number 1: ";
+    cin >> a;
+    cout << "Enter number 2: ";
+    cin >> b;
+    cout << "Enter number 3: ";
+    cin >> c;
+    cout << "The average is: " << (a + b + c) / 3;
+    return 0;
+}
