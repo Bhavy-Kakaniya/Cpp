dc568b0b1a328a09d3db18b417db2fd969bb17a8
Bhavy-Kakaniya <bhavy2kakaniya@gmail.com>
2025-11-11 22:35:29 +0530

    divsisble by 7 and not by 5

---
+++ Practice/05WhileLoop/07Divsisble7Not5.cpp
@@ -0,0 +1,15 @@
+// Print all integer greater than 100 and less than 200 that are divisible by 7 but not divisible by 5
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 100;
+    while (i < 200)
+    {
+        if ((i % 7 == 0) && (i % 5 != 0))
+            cout << i << endl;
+        i++;
+    }
+    return 0;
+}
