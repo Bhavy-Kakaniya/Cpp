3a6b2a97ce2ec7b71eebd52406a055ba44083555
Bhavy-Kakaniya <bhavy2kakaniya@gmail.com>
2025-11-17 22:31:24 +0530

    sum of n numbers

---
+++ Practice/06ForLoop/02SumToN.cpp
@@ -0,0 +1,13 @@
+// Sum of 1 to N
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, sum = 0;
+    cout << "Enter N: ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+        sum += i;
+    cout << "sum is " << sum;
+    return 0;
+}
