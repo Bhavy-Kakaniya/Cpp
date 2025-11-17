304e0cc8301a5b1832f4a3f9be295b642917e5e6
Bhavy-Kakaniya <bhavy2kakaniya@gmail.com>
2025-11-17 22:30:29 +0530

    print 1 to n

---
+++ Practice/06ForLoop/01OneToN.cpp
@@ -0,0 +1,13 @@
+// 1 to N
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter N: ";
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cout << i << " ";
+
+    return 0;
+}
