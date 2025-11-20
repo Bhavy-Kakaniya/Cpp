d440915e491315b7ba7d867c796a364726bb71bf
Bhavy-Kakaniya <bhavy2kakaniya@gmail.com>
2025-11-20 21:27:50 +0530

    pattern

---
+++ Practice/06ForLoop/13Pattern03.cpp
@@ -0,0 +1,21 @@
+// 5
+// 54
+// 543
+// 5432
+// 54321
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for (int i = 5; i > 0; i--)
+    {
+        for (int j = 5; j >= i; j--)
+        {
+            cout << j;
+        }
+        cout << endl;
+    }
+    return 0;
+}
