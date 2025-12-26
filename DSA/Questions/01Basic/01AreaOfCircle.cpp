9572666e083726c53f3ee3f4750a6eb0e4a930fb
Bhavy-Kakaniya <bhavy2kakaniya@gmail.com>
2025-12-26 22:46:28 +0530

    area of circle

---
+++ DSA/Questions/01Basic/01AreaOfCircle.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int radius;
+    cout << "Enter radius: ";
+    cin >> radius;
+    cout << "area is " << 3.14 * radius * radius;
+    return 0;
+}
